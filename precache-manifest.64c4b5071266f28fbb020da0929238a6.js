c25cd1bd5a3b4d88fa09f7a42c2f9521e78a4d83
Marcus Voß <meav33@gmail.com>
2020-07-03 15:56:45 +0200

    Updates

---
+++ precache-manifest.64c4b5071266f28fbb020da0929238a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70436d201eaec766233a9c184603d1e2",
+    "url": "/tsp-react/index.html"
+  },
+  {
+    "revision": "65ac62432a2a537420e2",
+    "url": "/tsp-react/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "79197d8410ad6bc37e11",
+    "url": "/tsp-react/static/css/main.807c1001.chunk.css"
+  },
+  {
+    "revision": "65ac62432a2a537420e2",
+    "url": "/tsp-react/static/js/2.054dfee9.chunk.js"
+  },
+  {
+    "revision": "e6625a03be1d7b7b54fbcd2102b0e67d",
+    "url": "/tsp-react/static/js/2.054dfee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79197d8410ad6bc37e11",
+    "url": "/tsp-react/static/js/main.e51eb4f6.chunk.js"
+  },
+  {
+    "revision": "7ae0684a7661c61612d3",
+    "url": "/tsp-react/static/js/runtime-main.6a5927ff.js"
+  },
+  {
+    "revision": "88ed7bc247e97df422786df9e773ec13",
+    "url": "/tsp-react/static/media/doubleCircle.88ed7bc2.png"
+  }
+]);
